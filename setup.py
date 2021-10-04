66c6bb0fa119f5512b0a470a3bc7eca454f05b84
SalvIac <52127188+SalvIac@users.noreply.github.com>
2021-10-04 16:29:16 +0100

    [20211004] Restructuring
    4 Oct 2021

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pysimulator',
+    version='0.0.1',
+    description='Simulator package for earthquake catalogs - Salvatore Iacoletti',
+    author='Salvatore Iacoletti',
+    author_email='salvatore.iacoletti92@gmail.com',
+    url='https://github.com/SalvIac/pysimulator',
+    packages=find_packages(),
+    zip_safe=False,
+    )
